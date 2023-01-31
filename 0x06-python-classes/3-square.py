575f026901eb706716873741bc60a81c3c659c5b
Njiraini23 <njiraini88@gmail.com>
2023-01-31 20:01:30 +0000

    Defines a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """
+    class defined with private 
+    instance attritbute: size
+    """
+    def __init__(self, size=0):
+        """
+        Args:
+            size: initialize size
+            """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Define area"""
+        return self.__size * self.__size
