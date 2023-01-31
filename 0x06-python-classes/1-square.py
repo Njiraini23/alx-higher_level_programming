3497c38c4de19620103aec00478989ef2536aa79
Njiraini23 <njiraini88@gmail.com>
2023-01-31 19:20:39 +0000

    class called squares

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""a class square that defines a square by:
+    Private instance attribute: size
+    instantiation with size(no type/value verification)
+    You are not allwoed to import any module
+"""
+
+
+class Square:
+    """a class named square"""
+    def __int__(self, size):
+        """initializing the object"""
+        self.__size = size
