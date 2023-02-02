442d7a30435b58a58809856abbd904cc6d016460
Njiraini23 <njiraini88@gmail.com>
2023-02-02 10:02:04 +0000

    A class square that defines a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""a class square that defines a square by:
+"""a class Square that defines a square by:
     Private instance attribute: size
-    instantiation with size(no type/value verification)
-    You are not allwoed to import any module
+    instantiation with size (no type/value verification)
+    You are not allowed to import any module
 """
 
 
 class Square:
-    """Represent a square."""
-    def __int__(self, size):
-        """__init__
-        The __int__method initializes the size value
-        of  the square.
-        Attritbues:
-            size (int): The size of the square.
-            """
+    """a class named square"""
+    def __init__(self, size):
+        """initializing the object"""
         self.__size = size
