b7c05f38a8fca677324863379b107e58812a1065
Njiraini23 <njiraini88@gmail.com>
2023-04-24 12:11:53 +0000

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-""" shows the states in the databse"""
-import mySQLdb
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    args = sys.argv
-    username = args[1]
-    password = args[2]
-    name = args[3]
-    # Initializing the DB
-    db = MySQLdb.connect(host='localhost', user=username,
-                         passwd=password, db=name, port=3306)
-    # Getting the cursor
-    curs = db.cursor()
-    # Executing the msql statement and exit from db
-    num_rows = curs.execute("""SELECT * FROM states WHERE name
-                    LIKE BINARY 'N%' ORDER BY states.id;""")
-    rows = curs.fetchall()
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    curs.close()
+    cur.close()
     db.close()
