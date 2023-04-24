77c7dbb83349a7b9677106241d36b7c606b0e645
Njiraini23 <njiraini88@gmail.com>
2023-04-24 10:01:28 +0000

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,18 +4,14 @@
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    args = sys.argv
-    my_user = argv[1]
-    my_pass = argv[2]
-    name = argv[3]
-    #DB iniliaization
-    db = MySQLdb.connect(host='localhost', user=my_user, passwd=my_pass,
-                         db=name, port=3306)
-    #Get the cursor
-    curs = db.cursor()
-    cur.execute('SELECT * FROM states ORDER BY states.id;')
-    result = cur.fetchall()
-    [print(row) for row in result]
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    """Get the cursor"""
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
