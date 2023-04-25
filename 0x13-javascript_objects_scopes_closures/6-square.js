8cecbe8ab55c842b8227dafb3e41917fb1874da4
Njiraini23 <njiraini88@gmail.com>
2023-04-25 07:15:07 +0000

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/*
+ *A class square that defines square and inherits
+ *from square of 5-square.js
+ */
+const SquareP = require('./5-square');
+
+class Square extends SquareP {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Square;
