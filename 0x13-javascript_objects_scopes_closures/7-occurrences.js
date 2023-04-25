8dc262be7b4ec9e031c16cc46103a948865dc1e8
Njiraini23 <njiraini88@gmail.com>
2023-04-25 07:40:37 +0000

    7-occurrences.js

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+//returns the number of the occurence in a list:
+exports.nbOccurences = function (list, searchEelement) {
+  let occurences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchEelement) {
+      occurences++;
+    }
+  }
+  return occurences;
+};
