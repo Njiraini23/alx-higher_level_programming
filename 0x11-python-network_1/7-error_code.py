d19aa90f40cb22b5c8cc7937f228344bbc974091
Njiraini23 <njirainii88@gmail.com>
2023-05-11 14:02:14 +0000

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A script that takes URL,sends a request to URL and 
+displays body of response. print error code of HTTP
+Status os greater thn or equal to 400
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+        else:
+        print(r.text)
