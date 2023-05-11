e362e61e2af084dd9d286483c0ce17725c12aec8
Njiraini23 <njirainii88@gmail.com>
2023-05-11 11:10:36 +0000

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,17 +3,16 @@
 Takes URL sends a request to the URL and displays
 the body of response decoded in utf-8).
 uses the packages urllib and sys
-Uses with statement
 """
-from sys import argv
 from urllib.request import urlopen
 from urllib.error import HTTPError
+from sys import argv
 
 
 if __name__ == "__main__":
     url = argv[1]
     try:
         with urlopen(url) as response:
-            print(respons.read().decode('utf-8'))
+            print(response.read().decode('utf-8'))
     except HTTPError as err:
         print('Error code: {}'.format(err.code))
