03620f5f832a7cf5ed9627c9d385b5aaa9a26f90
Njiraini23 <njirainii88@gmail.com>
2023-05-11 14:05:14 +0000

    changed files

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A script that takes URL,sends a request to URL and 
+A script that takes URL,sends a request to URL and
 displays body of response. print error code of HTTP
 Status os greater thn or equal to 400
 """
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     r = requests.get(url)
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
-        else:
+    else:
         print(r.text)
